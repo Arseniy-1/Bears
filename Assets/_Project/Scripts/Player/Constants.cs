682fca9a26fc7b0d1732d4b71d80886d46743f99
profftols <85066262+profftols@users.noreply.github.com>
2024-12-17 20:04:08 +0500

    Animation
    Remove
    - Old_Animation
    - Add_New_Animation

--- Assets/_Project/Scripts/Player/Constants.cs
+++ Assets/_Project/Scripts/Player/Constants.cs
@@ -8,10 +8,14 @@ static class Constants
         public const string IdleWithGun = nameof(IdleWithGun);
         public const string Run = nameof(Run);
         public const string RunWithGun = nameof(RunWithGun);
+        public const string TakeDamage = nameof(TakeDamage);
+        public const string ItemPickUp = nameof(ItemPickUp);
 
         public static readonly int ActivityAnimation = Animator.StringToHash(LongInactivity);
         public static readonly int IdleAnimation = Animator.StringToHash(IdleWithGun);
         public static readonly int RunningAnimation = Animator.StringToHash(Run);
         public static readonly int RunInWeaponAnimation = Animator.StringToHash(RunWithGun);
+        public static readonly int TakeDamageAnimation = Animator.StringToHash(TakeDamage);
+        public static readonly int ItemPickUpAnimation = Animator.StringToHash(ItemPickUp);
     }
 }
